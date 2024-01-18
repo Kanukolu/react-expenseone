656cc787953650d6e5635e71145ddae2aac0d402
kanukolu <kanukolulakshmi@gmail.com>
2024-01-18 17:31:05 +0530

    react app

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,12 @@
+function ExpenseItem() {
+    return (
+    <div>
+        
+        <div>
+            <p>foods 100 rs</p>
+           
+        </div>
+    </div>
+    )
+}
+export default ExpenseItem
