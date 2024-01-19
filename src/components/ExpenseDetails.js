9d6a14296056b0a2d7c2e52994826f36d149a7a6
kanukolu <kanukolulakshmi@gmail.com>
2024-01-19 11:02:59 +0530

    spilliting the components

---
+++ src/components/ExpenseDetails.js
@@ -0,0 +1,16 @@
+
+import  "./ExpenseItem.css";
+function ExpenseDetails(props){
+    const title=props.title;
+    const LocationOfExpenditure=props.LocationOfExpenditure;
+    const amount=props.amount;
+    return(
+        <div className='expense-item__description'>
+        <h2>{title}</h2>
+        <h2>{LocationOfExpenditure}</h2>
+        <div className='expense-item__price'>${amount}</div>
+      </div>
+
+    )
+}
+export default ExpenseDetails
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,21 +1,12 @@
+import ExpenseDate from './ExpenseDate';
+import ExpenseDetails from './ExpenseDetails';
 import './ExpenseItem.css';
 
-function ExpenseItem(Props) {
-  const expenseDate = new Date(2021, 2, 28);
-  const expenseTitle = 'Car Insurance';
-  const expenseAmount = 294.67;
-  const  LocationOfExpenditure='market'
-
+function ExpenseItem(props) {
   return (
     <div className='expense-item'>
-      <div>{Props.date.toISOString()}</div>
-      <div className='expense-item__description'>
-      
-        <h2>{Props.title}</h2>
-        <h2>{Props.LocationOfExpenditure}</h2> 
-        <div className='expense-item__price'>${Props.amount}</div>
-    </div>
-    
+      <ExpenseDate date={props.date} />
+      <ExpenseDetails title={props.title}LocationOfExpenditure={props.LocationOfExpenditure} amount={props.amount}/>
     </div>
   );
 }
