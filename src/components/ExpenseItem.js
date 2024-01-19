20cb7001b9ae41d88e8fd03f4100a4a6f7b30eaa
kanukolu <kanukolulakshmi@gmail.com>
2024-01-19 10:15:53 +0530

    parsing data through props

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,23 @@
-function ExpenseItem() {
-    return (
-    <div>
-        
-        <div>
-            <p>foods 100 rs</p>
-           
-        </div>
+import './ExpenseItem.css';
+
+function ExpenseItem(Props) {
+  const expenseDate = new Date(2021, 2, 28);
+  const expenseTitle = 'Car Insurance';
+  const expenseAmount = 294.67;
+  const  LocationOfExpenditure='market'
+
+  return (
+    <div className='expense-item'>
+      <div>{Props.date.toISOString()}</div>
+      <div className='expense-item__description'>
+      
+        <h2>{Props.title}</h2>
+        <h2>{Props.LocationOfExpenditure}</h2> 
+        <div className='expense-item__price'>${Props.amount}</div>
     </div>
-    )
+    
+    </div>
+  );
 }
-export default ExpenseItem
+
+export default ExpenseItem;
