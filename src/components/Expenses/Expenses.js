de2af123e9d4bc78d46d1210dc2d06104bab3a25
kanukolu <kanukolulakshmi@gmail.com>
2024-01-25 15:31:18 +0530

    using map for expenseitem

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -7,30 +7,14 @@ import './Expenses.css';
 const Expenses = (props) => {
   return (
     <Card className="expenses">
+      {props.items.map((expense)=>(
       <ExpenseItem
-        title={props.items[0].title}
-        LocationOfExpenditure={props.items[0].LocationOfExpenditure}
-        amount={props.items[0].amount}
-        date={props.items[0].date}
+        title={expense.title}
+        LocationOfExpenditure={expense.LocationOfExpenditure}
+        amount={expense.amount}
+        date={expense.date}
       />
-      <ExpenseItem
-        title={props.items[1].title}
-        LocationOfExpenditure={props.items[1].LocationOfExpenditure}
-        amount={props.items[1].amount}
-        date={props.items[1].date}
-      />
-      <ExpenseItem
-        title={props.items[2].title}
-        LocationOfExpenditure={props.items[2].LocationOfExpenditure}
-        amount={props.items[2].amount}
-        date={props.items[2].date}
-      />
-      <ExpenseItem
-        title={props.items[3].title}
-        LocationOfExpenditure={props.items[3].LocationOfExpenditure}
-        amount={props.items[3].amount}
-        date={props.items[3].date}
-      />
+      ))}
     </Card>
   );
 }
